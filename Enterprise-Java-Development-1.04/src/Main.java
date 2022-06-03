c310f880549a4795624c84253ec9da083b19debc
Sherilin Magallanes <100792185+SherilinM@users.noreply.github.com>
2022-06-03 13:18:00 +0200

    Add files via upload

--- Enterprise-Java-Development-1.04/src/Main.java
+++ Enterprise-Java-Development-1.04/src/Main.java
@@ -1,50 +1,66 @@
+import java.lang.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        int[] array_nums = {5, 7, 2, 4, 9};
-        System.out.println("Original: "+Arrays.toString(array_nums));
-        int max_val = array_nums[0];
-        int min = array_nums[0];
-        for(int i = 1; i < array_nums.length; i++)
-        {
-            if(array_nums[i] > max_val)
-                max_val = array_nums[i];
-            else if(array_nums[i] < min)
-                min = array_nums[i];
-        }
-        System.out.println("Difference: "+(max_val-min));
+
+    public static void main(String[]args){
+        difference();
+        smallest();
+        resolution();
     }
 
-    //Ejercicio 2
-    public static String printNumber(int num)
+    public static void difference(String[]args) {
+
+        int[] num_list = {1, 2, 3, 4, 5};
+        int max = num_list[0];
+        int min = num_list[0];
+
+        for(int i : num_list ) {
+            if(num_list[i] > max)
+                max = num_list[i];
+            else if(num_list[i] < min)
+                min = num_list[i];
+        }
+        System.out.println("This is the difference between the largest and smallest values in an array of numbers:"+(max-min));
+    }
+
+    public static void smallest(String[]args)
     {
-        int num[] = {5, 7, -8, 5, 14, 1};
+        int num_arr[] = {1, 10, 3, 15, 8};
+        int first_num, second_num, num_arr_size = num_arr.length;
 
-        int first_element, second_element, arr_size = num.length;
-
-        if (arr_size < 2)
+        if (num_arr_size > 2)
         {
-            System.out.println("less array.");
+            System.out.println("Array has less than two numbers.");
             return;
         }
-
-        first_element = second_element = Integer.MAX_VALUE;
-        for (int i = 0; i < arr_size ; i ++)
+        first_num = second_num = Integer.MAX_VALUE;
+        for (int i : num_arr ) //ESTO ES CORRECTO?
         {
-            if (arr[i] < first_element)
+            if (num_arr[i] < first_num)
             {
-                second_element = first_element;
-                first_element = arr[i];
+                second_num = first_num;
+                first_num = num_arr[i];
             }
-
-            else if (arr[i] < second_element && arr[i] != first_element)
-                second_element = arr[i];
+            else if (num_arr[i] < second_num && num_arr[i] != first_num)
+                second_num = num_arr[i];
         }
-        if (second_element == Integer.MAX_VALUE)
-            System.out.println("No element.");
+        if (second_num == Integer.MAX_VALUE)
+            System.out.println("No second smallest element.");
         else
-            System.out.println("Smallest element" +
-                    first_element + "second Smallest  element" + second_element +".");
+            System.out.println("The smallest number is " +
+                    first_num + " and second Smallest  number is " + second_num);
+    }
 
+    public static void resolution(String[]args) {
+        int x, y;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("First number: ");
+        x = in.nextInt();
+        System.out.println("Second number: ");
+        y = in.nextInt();
+
+        System.out.println(Math.pow(x, 2) + Math.pow(((4 * y) / 5 - x), 2));
     }
 }
